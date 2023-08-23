bbaf367692add2febbd238a9df2d7c2dad2fe41d
3rdlettercm <uschrismartinez@gmail.com>
2023-08-22 21:58:06 -0700

    initial commit

---
+++ primero.py
@@ -0,0 +1,33 @@
+print("Hello World");
+
+myString = "some string"
+anotherString = 'this too is a string'
+print(myString, anotherString);
+
+# numbers
+number = 83
+number2 = 83
+print("number");
+print(number);
+print(number, number2);
+
+#number as a String
+number_string = str(number) + " " + str(number2)
+print("The number as a string: ", number_string)
+
+#example
+#integer
+age = 99
+
+#string
+name = "Chris"
+
+print("My name is", name, "and I'm", age, " years old.");
+
+#basic math
+a = 24
+b = 29
+c = 27
+
+resultadoSuma = a + b + c
+print(resultadoSuma);
